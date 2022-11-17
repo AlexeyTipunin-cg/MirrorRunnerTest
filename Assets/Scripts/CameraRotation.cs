c9f703ddc2fb511799d3226f92780959341bc34a
Alexey Tipunin <alexeytipunincg@gmail.com>
2022-11-17 13:58:31 +0300

    default lobby + fixing bags in camerarot

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -14,15 +14,25 @@ public class CameraRotation : NetworkBehaviour
         _camera.gameObject.SetActive(true);
     }
 
+    [Command]
+    private void RotatePlayer(Vector2 rotation)
+    {
+        transform.eulerAngles = rotation;
+    }
+
     private Vector2 _rotation;
+
+    [ClientCallback]
     private void LateUpdate()
     {
         if (ApplicationController.blockInput) return;
-
-        _rotation.y += Input.GetAxis("Mouse X") * _lookSpeed * Time.deltaTime;
-        _rotation.x -= Input.GetAxis("Mouse Y") * _lookSpeed * Time.deltaTime;
-        _rotation.x = Mathf.Clamp(_rotation.x, -_lookXLimit, _lookXLimit);
-        _camera.localRotation = Quaternion.Euler(_rotation.x, 0, 0);
-        transform.eulerAngles = new Vector2(0, _rotation.y);
+        if (isLocalPlayer)
+        {
+            _rotation.y += Input.GetAxis("Mouse X") * _lookSpeed * Time.deltaTime;
+            _rotation.x -= Input.GetAxis("Mouse Y") * _lookSpeed * Time.deltaTime;
+            _rotation.x = Mathf.Clamp(_rotation.x, -_lookXLimit, _lookXLimit);
+            _camera.localRotation = Quaternion.Euler(_rotation.x, 0, 0);
+            RotatePlayer(new Vector2(0, _rotation.y));
+        }
     }
 }
