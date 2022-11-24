30064efc762e89c25d5d312299a858cb068fd111
Alexey Tipunin <alexeytipunincg@gmail.com>
2022-11-24 12:04:11 +0300

    some cleaning + fix Player transform update + update to new lts

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -6,15 +6,5 @@ using System;
 
 public class LobbyManager : NetworkRoomManager
 {
-    private const string GAMEPLAY_SCENE = "BattleScene";
-    public override void OnRoomServerAddPlayer(NetworkConnectionToClient conn)
-    {
- 
-        base.OnRoomServerAddPlayer(conn);
-        if (SceneManager.GetActiveScene().name == GAMEPLAY_SCENE)
-        {
-            var currentPlayerRoom = roomSlots.First(slot => slot.connectionToClient.connectionId == conn.connectionId) as RoomPlayer;
-            conn.identity.name = currentPlayerRoom.playerName;
-        }
-    }
+
 }
